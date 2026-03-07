ffc24021127e00530f09106bc59708e446f27e6c
Shaun <Shaunscheurer@gmail.com>
2026-03-06 23:05:11 -0600

    e

---
+++ IDKTBH/IDKTBH/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace MyApplication
+{
+    class Weapons
+    {
+        public string damage = "10";
+        public string distance = "5ft";
+        public string TOD = "peircing";
+
+        static void Main(string[] args)
+        {
+            Weapons Sword = new Weapons();
+            Weapons Javelin = new Weapons();
+            Console.WriteLine(Sword.damage + " " + Sword.distance + " " + Sword.TOD);
+            Console.WriteLine(Javelin.distance);
+        }
+    }
+}
---
+++ PlayingWithVariables/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+
+namespace YourNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b = 10, c = 15;
+            Console.WriteLine("Please enter a number");
+            int d = int.Parse(Console.ReadLine());
+
+            int min = Min(a, b);
+            int sum = Add(c, d);
+            int div = Division(a, b);
+            int times = Multiply(c, d); 
+            Console.WriteLine($"The minimum of {a} and {b} is {min}");
+            Console.WriteLine($"The addition of {c} and {d} is {sum}");
+            Console.WriteLine($"The division of {a} by {b} is {div}");
+            Console.WriteLine($"The multiplication of {c} and {d} is {times}");
+        }
+
+        static int Min(int x, int y)
+        {
+            return x < y ? x : y;
+        }
+
+        static int Add(int x, int y)
+        {                                          
+            return x + y;
+        }
+
+        static int Division(int x, int y)
+        {
+            return x / y;
+        }
+
+        static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
