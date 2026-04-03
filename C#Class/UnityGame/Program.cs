d49a773e95ad2ed034c2a99c0da6a0cb66e869eb
Shaun <Shaunscheurer@gmail.com>
2026-04-02 23:13:04 -0500

    Add new session file for GitHub Copilot chat interactions and performance profiling tools
    - Introduced a new session file to facilitate interactions with GitHub Copilot.
    - Added descriptions for performance profiling and modernization tools related to .NET projects.
    - Included guidance for users on when to utilize profiling tools and how to approach .NET upgrades and migrations.

--- C#Class/UnityGame/Program.cs
+++ C#Class/UnityGame/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.PerformanceData;
-using System.IO.MemoryMappedFiles;
 
 namespace ConsoleApp1
 {
@@ -11,47 +9,49 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
-            // Game setup
             bool running = true;
             int playerX = 2;
             int playerY = 2;
             int currentMap = 0;
 
-            string[] map1 = {
+            string[] map1 =
+            {
                 "##########",
                 "#......0.#",
-                "#0.......#",
-                "#..####..#",
-                "#..#.0#..#",
-                "#........]",
+                "#0.#####.#",
+                "#..#..#..#",
+                "#.##..#..#",
+                "#......0.]",
                 "##########"
             };
-            
-            string[] map2 = {
+
+            string[] map2 =
+            {
                 "##########",
                 "]........#",
-                "#..#...###",
+                "#..#.###.#",
                 "#...0.#.0#",
-                "#.#####..#",
+                "#.##.##..#",
                 "#....0...]",
                 "##########"
             };
 
-            string[] map3 = { 
+            string[] map3 =
+            {
                 "###############################",
-                "].........0...........#......##",
-                "###.#.#####.#.#######.#.####.##",
-                "#...#.#...#.#.#.....#.#....#..#",
-                "#.##.#.#.#.#.#.#####.#.###.##.#",
-                "#.#.0...#...#...#..........#..#",
-                "#.#.#####.#######.#######.##.##",
-                "#.#.#...0...#.......#...#.#.0.#",
-                "#...#.#.###.#.......#.###.#.#.#",
-                "#####.#...#.#.......#.#...#.#.#",
+                "].........0....#......#......##",
+                "###.#######.#######.#.#.####.##",
+                "#......#....#.,.....#.0....#.0#",
+                "#.####.###.###.####.######.##.#",
+                "#.#.0...#.......#..........#..#",
+                "#.#######.#######.#######.##.##",
+                "#.#0#...0...#..#..#0#.....#.0.#",
+                "#...#.#.###.#..#.##.#.###.#.#.#",
+                "#####.#...#.#.#..#..#.#...#.#.#",
                 "#.....###.#.#.......#.#.###.#.#",
-                "#.#######.#.#.......#.#.#...#.#",
+                "#.#######.#.#..0.#..#.#.#...#.#",
                 "#.#.....#.#.#########.#.#.###.#",
-                "#...###.0.#...........#...0.../",
+                "#...###.0.#......0....#...0.../",
                 "###############################"
             };
 
@@ -60,73 +60,38 @@ namespace ConsoleApp1
             mapList.Add(map2);
             mapList.Add(map3);
 
-            int enemy_x = 5;
-            int enemy_y = 5;
+            int enemyX = 5;
+            int enemyY = 5;
 
-            // Main game loop
             while (running)
             {
                 Console.Clear();
 
                 string[] activeMap = mapList[currentMap];
 
-                // Draw map
                 Console.SetCursorPosition(0, 0);
                 foreach (string line in activeMap)
                 {
                     Console.WriteLine(line);
                 }
+
                 Console.SetCursorPosition(0, activeMap.Length + 1);
                 Console.WriteLine("Coins: " + coins);
 
-                // Draw entities
                 Console.SetCursorPosition(playerX, playerY);
                 Console.Write("@");
 
-                Console.SetCursorPosition(enemy_x, enemy_y);
+                Console.SetCursorPosition(enemyX, enemyY);
                 Console.Write("E");
 
-                // Input handling
                 ConsoleKeyInfo key = Console.ReadKey(true);
-                
-                if (key.Key == ConsoleKey.Escape) 
+
+                if (key.Key == ConsoleKey.Escape)
                 {
                     running = false;
                     continue;
                 }
 
-                // Enemy AI: Move toward player
-                int enemyNextX = enemy_x;
-                int enemyNextY = enemy_y;
-               
-                if (playerX == enemy_x && playerY == enemy_y)
-                {
-                    Console.Clear();
-                    Console.WriteLine("You died!");
-                    Console.ReadKey(true);
-                    break;
-                }
-
-                if (enemy_x < playerX) enemyNextX++;
-                else if (enemy_x > playerX) enemyNextX--;
-
-                if (enemy_y < playerY) enemyNextY++;
-                else if (enemy_y > playerY) enemyNextY--;
-
-                // Enemy collision check
-                if (enemyNextY >= 0 && enemyNextY < activeMap.Length &&
-                    enemyNextX >= 0 && enemyNextX < activeMap[enemyNextY].Length)
-                {
-                    char enemyTile = activeMap[enemyNextY][enemyNextX];
-
-                    if (enemyTile != '#')
-                    {
-                        enemy_x = enemyNextX;
-                        enemy_y = enemyNextY;
-                    }
-                }
-                
-                // Calculate player next position
                 int nextX = playerX;
                 int nextY = playerY;
 
@@ -135,49 +100,167 @@ namespace ConsoleApp1
                 if (key.Key == ConsoleKey.A) nextX--;
                 if (key.Key == ConsoleKey.D) nextX++;
 
-                // Player bounds and collision check
-                if (nextY >= 0 && nextY < activeMap.Length &&
-                    nextX >= 0 && nextX < activeMap[nextY].Length)
+                if (IsInsideMap(activeMap, nextX, nextY))
                 {
                     char nextTile = activeMap[nextY][nextX];
 
-                    // Transition to next map
-                    if (nextTile == ']')
+                    if (nextTile == '#')
+                    {
+                        // blocked by wall, do nothing
+                    }
+                    else if (nextTile == ']')
                     {
                         currentMap++;
                         if (currentMap >= mapList.Count)
+                        {
                             currentMap = 0;
+                        }
 
-                        playerX = 2;
-                        playerY = 2;
+                        activeMap = mapList[currentMap];
+
+                        if (!TryFindTile(activeMap, ']', out playerX, out playerY))
+                        {
+                            playerX = 1;
+                            playerY = 1;
+                        }
+
+                        if (!TryFindEnemySpawn(activeMap, playerX, playerY, out enemyX, out enemyY))
+                        {
+                            enemyX = 1;
+                            enemyY = 1;
+                        }
+
+                        if (enemyX == playerX && enemyY == playerY)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("You died!");
+                            Console.ReadKey(true);
+                            break;
+                        }
+
+                        continue;
                     }
-                    else if (nextTile != '#')
+                    else if (nextTile == '/')
                     {
                         playerX = nextX;
                         playerY = nextY;
-                    }
 
-                    // Win condition
-                    if (nextTile == '/')
-                    {
                         Console.Clear();
                         Console.WriteLine("Congratulations! You've completed the game!");
                         Console.ReadKey(true);
                         running = false;
+                        continue;
                     }
-
-                    // Collectibles
-                    if (nextTile == '0')
+                    else
                     {
-                        coins++;
+                        playerX = nextX;
+                        playerY = nextY;
 
-                        string row = activeMap[nextY];
-                        char[] rowArray = row.ToCharArray();
-                        rowArray[nextX] = '.';
-                        activeMap[nextY] = new string(rowArray);
+                        if (nextTile == '0')
+                        {
+                            coins++;
+
+                            string row = activeMap[nextY];
+                            char[] rowArray = row.ToCharArray();
+                            rowArray[nextX] = '.';
+                            activeMap[nextY] = new string(rowArray);
+                        }
+                    }
+                }
+
+                if (enemyX == playerX && enemyY == playerY)
+                {
+                    Console.Clear();
+                    Console.WriteLine("You died!");
+                    Console.ReadKey(true);
+                    break;
+                }
+
+                int enemyNextX = enemyX;
+                int enemyNextY = enemyY;
+
+                if (enemyX < playerX) enemyNextX++;
+                else if (enemyX > playerX) enemyNextX--;
+
+                if (enemyY < playerY) enemyNextY++;
+                else if (enemyY > playerY) enemyNextY--;
+
+                if (IsInsideMap(activeMap, enemyNextX, enemyNextY))
+                {
+                    char enemyTile = activeMap[enemyNextY][enemyNextX];
+
+                    if (enemyTile != '#')
+                    {
+                        enemyX = enemyNextX;
+                        enemyY = enemyNextY;
+                    }
+                }
+
+                if (enemyX == playerX && enemyY == playerY)
+                {
+                    Console.Clear();
+                    Console.WriteLine("You died!");
+                    Console.ReadKey(true);
+                    break;
+                }
+            }
+        }
+
+        private static bool IsInsideMap(string[] map, int x, int y)
+        {
+            return y >= 0 && y < map.Length && x >= 0 && x < map[y].Length;
+        }
+
+        private static bool TryFindTile(string[] map, char tile, out int x, out int y)
+        {
+            for (int row = 0; row < map.Length; row++)
+            {
+                for (int col = 0; col < map[row].Length; col++)
+                {
+                    if (map[row][col] == tile)
+                    {
+                        x = col;
+                        y = row;
+                        return true;
                     }
                 }
             }
+
+            x = -1;
+            y = -1;
+            return false;
+        }
+
+        private static bool TryFindEnemySpawn(string[] map, int playerX, int playerY, out int spawnX, out int spawnY)
+        {
+            spawnX = -1;
+            spawnY = -1;
+
+            int bestDistance = -1;
+
+            for (int row = 0; row < map.Length; row++)
+            {
+                for (int col = 0; col < map[row].Length; col++)
+                {
+                    if (map[row][col] != '.')
+                    {
+                        continue;
+                    }
+
+                    int dx = col - playerX;
+                    int dy = row - playerY;
+                    int distance = dx * dx + dy * dy;
+
+                    if (distance > bestDistance)
+                    {
+                        bestDistance = distance;
+                        spawnX = col;
+                        spawnY = row;
+                    }
+                }
+            }
+
+            return bestDistance >= 0;
         }
     }
 }
